bbeff0ac6ad53843f0d337f5ff6142a26a30c33f
Mehmet Fatih <mfatihapaydin@outlook.com>
2024-01-11 12:30:07 +0300

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from './../utils/error.js';
+import  jwt  from 'jsonwebtoken';
+
 export const signup =  async(req, res, next) => {
     const {username, email, password}= req.body;
     const hashedPassword = bcryptjs.hashSync(password , 10 );
@@ -10,5 +13,19 @@ export const signup =  async(req, res, next) => {
     } catch (error) {
         next(error);
     }
-    
 };
+
+export const signin = async (req, res, next) =>{
+    const {email, password } = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User  not found!'));
+        const valiPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!valiPassword) return next(errorHandler(404, 'Wron cridentials !'));
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+        const {password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token',token , {httpOnly: true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
